7b89c47b4b93da3793cb7e791661aca6a74aa7e7
KoryunWeConstruct <koryun986@gmail.com>
2024-03-06 19:28:10 +0400

    feat(user entity): create user entity

--- src/database/models/user.ts
+++ src/database/models/user.ts
@@ -1,15 +1,32 @@
-import { Model, DataType, InferAttributes, InferCreationAttributes, CreationOptional } from "@sequelize/core";
+import { Model, DataType, InferAttributes, InferCreationAttributes, CreationOptional, DataTypes } from "@sequelize/core";
+//@ts-ignore
+import { Attribute, PrimaryKey, AutoIncrement, NotNull, Default } from '@sequelize/core/decorators-legacy';
+
 import { UserAttributes } from "../attributes/user-attributes";
 export class User extends Model<UserAttributes> {
-    
+    @Attribute(DataTypes.INTEGER)
+    @PrimaryKey
+    @AutoIncrement
     declare id: number;
 
-    first_name!: string;
+    @Attribute(DataTypes.STRING)
+    @NotNull
+    declare first_name: string;
 
-    
-    last_name!: string;
+    @Attribute(DataTypes.STRING)
+    @NotNull
+    declare last_name: string;
 
-    email!: string;
+    @Attribute(DataTypes.STRING)
+    @NotNull
+    declare email: string;
     
-    password!: string;
+    @Attribute(DataTypes.STRING)
+    @NotNull
+    declare password: string;
+
+    @Attribute(DataTypes.BOOLEAN)
+    @NotNull
+    @Default(false)
+    declare is_activated: CreationOptional<boolean>;
 }
