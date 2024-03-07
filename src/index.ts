d8fe354c28c000b9ce307d3b6d9aac7dd09e093b
KoryunWeConstruct <koryun986@gmail.com>
2024-03-07 17:26:47 +0400

    feat(cookies)

--- src/index.ts
+++ src/index.ts
@@ -3,13 +3,15 @@ import cors from "cors";
 import { PORT } from "./config/envirenmentVariables";
 import sequelize from "./database";
 import authRouter from "./routes/auth-routes";
+import cookieParser from "cookie-parser";
 
 const app: Express = express();
 
 app.use(cors({
     origin: "*",
+    credentials: true,
 }));
-
+app.use(cookieParser());
 app.use(express.json());
 
 app.use("/auth/", authRouter);
