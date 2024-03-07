c388dd07c8ef81bc13226976f34b8352f0a59b07
KoryunWeConstruct <koryun986@gmail.com>
2024-03-07 16:38:10 +0400

    feat(activation link entity)

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,6 +1,8 @@
 import { Sequelize } from "@sequelize/core";
 import { DATABASE_NAME, DATABASE_HOST, DATABASE_USERNAME, DATABASE_PASSWORD } from '../config/envirenmentVariables';
 import { User } from "./models/user";
+import { Token } from "./models/token";
+import { UserActivationLink } from "./models/user-activation-link";
 
 const sequelize = new Sequelize(
   DATABASE_NAME!,
@@ -10,7 +12,7 @@ const sequelize = new Sequelize(
   host: DATABASE_HOST,
   password: DATABASE_PASSWORD,
   dialect: "postgres",
-  models: [User]
+  models: [User, Token, UserActivationLink]
 });
 
 export default sequelize;
