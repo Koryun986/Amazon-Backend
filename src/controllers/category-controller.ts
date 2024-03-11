9b127f1625297cbbcbacae189e4fe306b40084af
KoryunWeConstruct <koryun986@gmail.com>
2024-03-11 19:36:06 +0400

    feat(create category)

---
+++ src/controllers/category-controller.ts
@@ -0,0 +1,24 @@
+import { Request, Response, NextFunction } from "express";
+import categoryService from "./../services/category-service";
+
+class CategoryController {
+    async getCategories(req: Request, res: Response, next: NextFunction) {
+        try {
+            const categories = await categoryService.getCategories();
+            return res.json(categories);
+        } catch (e) {
+            next(e);
+        }
+    }
+
+    async createCategory(req: Request, res: Response, next: NextFunction) {
+        try {
+            const category = await categoryService.createCategory(req.body);
+            return res.json(category);
+        } catch (e) {
+            next(e);
+        }
+    }
+}
+
+export default new CategoryController();
--- src/routes/category-route.ts
+++ src/routes/category-route.ts
@@ -1,7 +1,10 @@
 import {Router} from "express";
+import categoryController from "./../controllers/category-controller";
+import {adminGuard, authGuard} from "../middlewares/auth-middleware";
 
 const router = Router();
 
-
+router.get("/", categoryController.getCategories);
+router.post("/", authGuard, adminGuard, categoryController.createCategory);
 
 export default router;
