cf59837cc8b0c2144a7368b855183a796eba496a
KoryunWeConstruct <koryun986@gmail.com>
2024-03-12 16:11:27 +0400

    feat(get sizes)

--- src/controllers/size-controller.ts
+++ src/controllers/size-controller.ts
@@ -1,8 +1,14 @@
 import { Request, Response, NextFunction } from "express";
+import sizeService from "./../services/size-service";
 
 class SizeController {
     async getSizes(req: Request, res: Response, next: NextFunction) {
-
+        try {
+            const sizes = await sizeService.getSizes();
+            return res.json(sizes);
+        } catch (e) {
+            next(e);
+        }
     }
 }
 
--- src/services/size-service.ts
+++ src/services/size-service.ts
@@ -1,5 +1,13 @@
-class SizeService {
+import {Size} from "../database/models/size";
 
+class SizeService {
+    async getSizes() {
+        const sizeEntities = await Size.findAll();
+        if (!sizeEntities) {
+            return [];
+        }
+        return sizeEntities;
+    }
 }
 
 export default new SizeService();
