f3b0c4c9b8db03a6dbb2e911a5bb8834cb1ad7de
KoryunWeConstruct <koryun986@gmail.com>
2024-03-13 13:08:26 +0400

    feat(get route favorite products)

--- src/controllers/favorite-products-controller.ts
+++ src/controllers/favorite-products-controller.ts
@@ -1,5 +1,9 @@
-class FavoriteProductsController {
+import { Request, Response, NextFunction } from "express";
 
+class FavoriteProductsController {
+    async getFavorites(req: Request, res: Response, next: NextFunction) {
+
+    }
 }
 
 export default new FavoriteProductsController();
--- src/routes/favorite-products-route.ts
+++ src/routes/favorite-products-route.ts
@@ -1,5 +1,13 @@
 import {Router} from "express";
+import {authGuard} from "../middlewares/auth-middleware";
+import favoriteProductsController from "./../controllers/favorite-products-controller";
 
 const router = Router();
+
+router.get(
+    "/",
+    authGuard,
+    favoriteProductsController.getFavorites
+);
 
 export default router;
