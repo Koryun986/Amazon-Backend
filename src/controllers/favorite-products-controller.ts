f536165eb7aa6a21c83ae01b6879014f396baf0e
KoryunWeConstruct <koryun986@gmail.com>
2024-03-13 13:17:43 +0400

    feat(get favorite products)

--- src/controllers/favorite-products-controller.ts
+++ src/controllers/favorite-products-controller.ts
@@ -1,8 +1,15 @@
 import { Request, Response, NextFunction } from "express";
+import favoriteProductsService from "./../services/favorite-products-service";
 
 class FavoriteProductsController {
     async getFavorites(req: Request, res: Response, next: NextFunction) {
-
+        try {
+            //@ts-ignore
+            const favorites = await favoriteProductsService.getFavorites(req.user);
+            return res.json(favorites);
+        } catch (e) {
+            next(e);
+        }
     }
 }
 
--- src/services/favorite-products-service.ts
+++ src/services/favorite-products-service.ts
@@ -1,5 +1,16 @@
-class FavoriteProductsService {
+import type {UserDto} from "../dtos/user-dto";
+import {FavoriteProduct} from "../database/models/favorite-prodcut";
+import {User} from "../database/models/user";
 
+class FavoriteProductsService {
+    async getFavorites(userDto: UserDto) {
+        const userEntity = await User.findOne({where: {email: userDto.email}});
+        if (!userEntity) {
+            throw new Error("UnAuthorized Error");
+        }
+        const favoriteEntities = await FavoriteProduct.findAll({where: {user_id: userEntity.id}});
+        return favoriteEntities;
+    }
 }
 
 export default new FavoriteProductsService();
