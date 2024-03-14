aaebb9e8c2d6223a79db11192aed0c08e49e83fc
KoryunWeConstruct <koryun986@gmail.com>
2024-03-14 16:05:17 +0400

    feat(cart item): define controller and service

---
+++ src/controllers/cart-item-controller.ts
@@ -0,0 +1,7 @@
+import cartItemService from "../services/cart-item-service";
+
+class CartItemController {
+
+}
+
+export default new CartItemController();
---
+++ src/services/cart-item-service.ts
@@ -0,0 +1,5 @@
+class CartItemService {
+
+}
+
+export default new CartItemService();
