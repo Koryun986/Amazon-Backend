ae1356e71d39ad91af8a93a56fb0b7c67014e32b
KoryunWeConstruct <koryun986@gmail.com>
2024-03-21 19:55:17 +0400

    feat(favroite products and user association)

--- src/database/models/favorite-prodcut.ts
+++ src/database/models/favorite-prodcut.ts
@@ -1,5 +1,13 @@
-import { Model, DataTypes, InferAttributes, InferCreationAttributes, CreationOptional } from "@sequelize/core";
-import { Table, Attribute, PrimaryKey, AutoIncrement, NotNull } from "@sequelize/core/decorators-legacy";
+import {
+    Model,
+    DataTypes,
+    InferAttributes,
+    InferCreationAttributes,
+    CreationOptional,
+    NonAttribute
+} from "@sequelize/core";
+import { Table, Attribute, PrimaryKey, AutoIncrement, NotNull, BelongsTo } from "@sequelize/core/decorators-legacy";
+import {User} from "./user";
 
 @Table({tableName: "Favorite_Products", createdAt: false, updatedAt: false})
 export class FavoriteProduct extends Model<InferAttributes<FavoriteProduct>, InferCreationAttributes<FavoriteProduct>> {
@@ -15,4 +23,7 @@ export class FavoriteProduct extends Model<InferAttributes<FavoriteProduct>, Inf
     @Attribute(DataTypes.INTEGER)
     @NotNull
     product_id: number;
+
+    @BelongsTo(() => User, "user_id")
+    declare user?: NonAttribute<User>
 }
