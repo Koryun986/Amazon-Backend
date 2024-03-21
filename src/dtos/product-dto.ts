5d76275df35ed0b49ed313ab279e9d39c3476fa4
KoryunWeConstruct <koryun986@gmail.com>
2024-03-21 20:29:09 +0400

    refactor(product return type)

--- src/dtos/product-dto.ts
+++ src/dtos/product-dto.ts
@@ -1,7 +1,7 @@
 import {InferAttributes} from "@sequelize/core";
 import {Product} from "../database/models/product";
 
-export type ProductDto = Omit<InferAttributes<Product>, "id" | "color_id" | "category_id" | "size_id" | "total_earnings" | "time_bought" | "owner_id"> & {
+export type ProductDto = Omit<InferAttributes<Product>, "id" | "color_id" | "category_id" | "size_id" | "total_earnings" | "time_bought" | "owner_id" | "images"> & {
     color: string;
     category: string;
     size: string;
