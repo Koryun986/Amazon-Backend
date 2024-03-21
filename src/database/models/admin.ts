268f41469119d746c9cf2f8f8a3c9e39be6d5afc
KoryunWeConstruct <koryun986@gmail.com>
2024-03-21 19:42:30 +0400

    feat(user admin association

--- src/database/models/admin.ts
+++ src/database/models/admin.ts
@@ -1,5 +1,13 @@
-import { Model, DataTypes, InferAttributes, InferCreationAttributes, CreationOptional } from "@sequelize/core";
-import { Table, Attribute, PrimaryKey, AutoIncrement, NotNull } from "@sequelize/core/decorators-legacy";
+import {
+    Model,
+    DataTypes,
+    InferAttributes,
+    InferCreationAttributes,
+    CreationOptional,
+    NonAttribute
+} from "@sequelize/core";
+import { Table, Attribute, PrimaryKey, AutoIncrement, NotNull, BelongsTo } from "@sequelize/core/decorators-legacy";
+import {User} from "./user";
 
 @Table({tableName: "Admins"})
 export class Admin extends Model<InferAttributes<Admin>, InferCreationAttributes<Admin>> {
@@ -11,4 +19,7 @@ export class Admin extends Model<InferAttributes<Admin>, InferCreationAttributes
     @Attribute(DataTypes.INTEGER)
     @NotNull
     declare user_id: number;
+
+    @BelongsTo(() => User, "user_id")
+    declare user?: NonAttribute<User>
 }
